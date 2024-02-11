7b976dadc2dc6cb3ab56ba644d4019b36620f943
Jasur0999 <leaderjasur@gmail.com>
2024-02-11 13:16:38 +0500

    sgbrgj

---
+++ main.js
@@ -0,0 +1,45 @@
+//  1- task  //////////////////////////
+
+// const me = {
+//     firstName: "Sultonqul",
+//     lastName: "Nortoyloqov",
+//     age: 23,
+//     languages: ["js", "python", "c++", "nodejs"],
+//     friends: ["Jamshid", "Abbos", "Jalol", "Mar'uf"],
+//   };
+
+//   for (let key in me) {
+//     if (Array.isArray(me[key])) {
+//       console.log(`${key}: ${me[key].join(",")}`);
+//     } else {
+//       console.log(`${key}: ${me[key]}`);
+//     }
+//   }
+
+//  2- task  //////////////////////////
+
+// function createSquaredObject(n) {
+//   let squaredObject = {};
+//   for (let i = 1; i <= n; i++) {
+//     squaredObject[i] = i * i;
+//   }
+//   return squaredObject;
+// }
+// console.log(createSquaredObject(5));
+
+//  3- task  //////////////////////////
+
+// let misolObject = {
+//     name: 'John',
+//     age: 25,
+//     city: 'New York'
+//   };
+  
+//   let kalitlar = Object.keys(misolObject); 
+//   let qiymatlar = Object.values(misolObject); 
+  
+//   console.log(kalitlar); 
+//   console.log(qiymatlar); 
+  
+//   let yigindi = kalitlar.concat(qiymatlar); 
+//   console.log(yigindi); 
